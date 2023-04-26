a487d9c4a6487d3aa0634e20cbc8244ff313f073
donlHenard <pastor.git101@gmail.com>
2023-04-26 15:12:06 +0800

    Change the map to OSM

---
+++ js/city_hall.js
@@ -0,0 +1,20 @@
+
+let map = L.map('map').setView([15.480288, 120.588008], 16);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+let marker = L.marker([15.480288, 120.588008]).addTo(map);
+
+let popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
