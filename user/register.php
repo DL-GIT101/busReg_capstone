869a1287ce951e53af44dbb893fd3baa1f320c1c
donlHenard <pastor.git101@gmail.com>
2023-05-02 12:07:16 +0800

    Add validation and frontend validation message
    My vs code has a bit of a problem it does not track my changes but the
    git bash still tracking it

--- user/register.php
+++ user/register.php
@@ -1,4 +1,32 @@
+<?php
 
+$email = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// email
+    $email = validate($_POST["email"]);
+        if(empty($email)){
+            $email_error = "Enter Email";
+        }else{
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $email_error = "Invalid Email";
+            }else{ 
+               /* $emailSQL = "SELECT * FROM user WHERE email = '$email'";
+                $emailRESULT = $conn->query($emailSQL);
+                    if($emailRESULT->num_rows==1){
+                        $email_error = "Email was already taken";
+                    } */
+            }
+        }
+}
+
+function validate($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+        return $data;
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,6 +35,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/form.js" defer></script>
     <title>Register</title>
 </head>
 <body>
@@ -32,7 +61,8 @@
             </div>
             <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
                 <label for="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="Email Address">
+                <input type="email" id="email" name="email" placeholder="Email Address" value=<?php echo $email; ?>>
+                <div class="error">error</div>
 
                 <label for="password">Password</label>
                 <input type="password" id="password" name="password" placeholder="Password">
@@ -45,6 +75,5 @@
             </form>
         </div>
      </div>
-
 </body>
 </html>
