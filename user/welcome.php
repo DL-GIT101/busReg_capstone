6dfb707743b4387ceec510519ea8b5f28d4f9ab4
donlHenard <pastor.git101@gmail.com>
2023-05-07 17:07:39 +0800

    Add temp map

--- user/welcome.php
+++ user/welcome.php
@@ -5,7 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
-    <script src="../js/form.js" defer></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+     integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+     crossorigin=""/>
+     <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+     integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+     crossorigin=""></script>
+    <script src="../js/displayMap.js" defer></script>
     <title>Welcome</title>
 </head>
 <body>
@@ -53,11 +59,16 @@
                     <a href="" class="item title services">
                       <span class="emphasize">New</span> Business Permit
                     </a>
-                    <div class="item title services">
+                    <a href="" class="item title services">
                       <span class="emphasize">Renew</span> Business Permit
+                    </a>
+                </div>
+                <div class="frame location">
+                    <p class="title">Location</p>
+                    <div id="map">
+                        <!--temp- map -->
                     </div>
                 </div>
-                <div class="frame"></div>
             </div>
         </div>
     </div>
