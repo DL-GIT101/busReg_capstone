89ac6741bcad376b27e9ad12e5a72166c82a00b8
donlHenard <pastor.git101@gmail.com>
2023-05-09 18:53:14 +0800

    Rearrange a form and add new class group

--- user/profile.php
+++ user/profile.php
@@ -44,148 +44,178 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 
                 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             <div class="row">
+                <div class="frame wide">
+                    <div class="intro">
+                        <p class="title">Profile</p>
+                        <p class="sentence">Enter your informations to make a profile</p>
+                    </div>
                 <!--Owner -->
-                <div class="frame">
-                <p class="title">Profile</p>
-                <p class="sentence text-center">Enter your informations to make a profile</p>
                     <p class="title">Owner</p>
-                    <label for="owner_name">Name</label>
-                    <input type="text" id="owner_name" name="owner_name" placeholder="Owner Name" value="">
-                    <div class="error"></div>
-
-                    <label for="gender">Gender</label>
-                    <select name="gender" id="gender">
-                        <option value="" disabled selected>Select Gender..</option>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                    </select>
-                    <div class="error"></div>
-                </div>
-                <!--BUSINESS -->
-                <div class="frame">
-                    <p class="title">Business</p>
-                    <label for="bus_name">Name</label>
-                    <input type="text" id="bus_name" name="bus_name" placeholder="Business Name" value="">
-                    <div class="error"></div>
-
-                    <label for="logo">Logo</label>
-                    <input type="file" id="logo" name="logo">
-                    <div class="error"></div>
-
-                    <label for="activity">Activity</label>
-                    <input type="text" id="activity" name="activity" placeholder="Business Activity" value="">
-                    <div class="error"></div>
-
-                    <label for="contact">Contact Number</label>
-                    <input type="text" id="contact" name="contact" placeholder="Contact Number" value="">
-                    <div class="error"></div>
+                    <div class="row">
+                        <div class="group">
+                            <label for="first_name">First Name</label>
+                            <input type="text" id="first_name" name="first_name" placeholder="First Name" value="">
+                            <div class="error"></div>
+                        </div>
+                        <div class="group">
+                            <label for="middle_name">Middle Name</label>
+                            <input type="text" id="middle_name" name="middle_name" placeholder="Middle Name" value="">
+                            <div class="error"></div>
+                        </div>
+                        <div class="group">
+                            <label for="surname">Surname</label>
+                            <input type="text" id="surname" name="surname" placeholder="Surname" value="">
+                            <div class="error"></div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="group">
+                            <label for="suffix">Suffix</label>
+                            <input type="text" id="suffix" name="suffix" placeholder="suffix" value="">
+                            <div class="error"></div>
+                        </div>
+                        <div class="group">
+                            <label for="gender">Gender</label>
+                            <select name="gender" id="gender">
+                                <option value="" disabled selected>Select Gender..</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </select>
+                            <div class="error"></div>
+                        </div>
+                    </div>
                     
-                </div>
-                <!--address -->
-                <div class="frame">
-                    <p class="title">Address</p>
-
-                    <label for="address_1">House No./Unit No./Building/Street</label>
-                    <input type="text" id="address_1" name="address_1" placeholder="House No./Unit No./Building/Street" value="">
-                    <div class="error"></div>
-
-                    <label for="address_2">Barangay</label>
-                    <select id="address_2" name="address_2">
-                        <option value="" disabled selected>Select Barangay...</option>
-                        <?php
-                            $barangays = array(
-                                'Aguso',
-                                'Alvindia',
-                                'Amucao',
-                                'Armenia',
-                                'Asturias',
-                                'Atioc',
-                                'Balanti',
-                                'Balete',
-                                'Balibago I',
-                                'Balibago II',
-                                'Balingcanaway',
-                                'Banaba',
-                                'Bantog',
-                                'Baras-baras',
-                                'Batang-batang',
-                                'Binauganan',
-                                'Bora',
-                                'Buenavista',
-                                'Buhilit',
-                                'Burot',
-                                'Calingcuan',
-                                'Capehan',
-                                'Carangian',
-                                'Care',
-                                'Central',
-                                'Culipat',
-                                'Cut-cut I',
-                                'Cut-cut II',
-                                'Dalayap',
-                                'Dela Paz',
-                                'Dolores',
-                                'Laoang',
-                                'Ligtasan',
-                                'Lourdes',
-                                'Mabini',
-                                'Maligaya',
-                                'Maliwalo',
-                                'Mapalacsiao',
-                                'Mapalad',
-                                'Matatalaib',
-                                'Paraiso',
-                                'Poblacion',
-                                'Salapungan',
-                                'San Carlos',
-                                'San Francisco',
-                                'San Isidro',
-                                'San Jose',
-                                'San Jose de Urquico',
-                                'San Juan Bautista (formerly Matadero)',
-                                'San Juan de Mata (formerly Malatiki)',
-                                'San Luis',
-                                'San Manuel',
-                                'San Miguel',
-                                'San Nicolas',
-                                'San Pablo',
-                                'San Pascual',
-                                'San Rafael',
-                                'San Roque',
-                                'San Sebastian',
-                                'San Vicente',
-                                'Santa Cruz',
-                                'Santa Maria',
-                                'Santo Cristo',
-                                'Santo Domingo',
-                                'Santo Niño',
-                                'Sapang Maragul',
-                                'Sapang Tagalog',
-                                'Sepung Calzada (Panampunan)',
-                                'Sinait',
-                                'Suizo',
-                                'Tariji',
-                                'Tibag',
-                                'Tibagan',
-                                'Trinidad',
-                                'Ungot',
-                                'Villa Bacolor'
-                            );
-                            foreach ($barangays as $barangay){
-                                echo "<option value='$barangay'>$barangay</option>";
-                            }
-                        ?>
-                    </select>
-                    <div class="error"></div>
+                <!--BUSINESS -->
+                     <p class="title">Business</p>
+                     <div class="row">
+                        <div class="group">
+                            <label for="bus_name">Name</label>
+                            <input type="text" id="bus_name" name="bus_name" placeholder="Business Name" value="">
+                            <div class="error"></div>
+                        </div>
+                        <div class="group">
+                            <label for="logo">Logo</label>
+                            <input type="file" id="logo" name="logo">
+                            <div class="error"></div>
+                        </div>
+                     </div>
+                     <div class="row">
+                        <div class="group">
+                            <label for="activity">Activity</label>
+                            <input type="text" id="activity" name="activity" placeholder="Business Activity" value="">
+                            <div class="error"></div>
+                        </div>
+                        <div class="group">
+                            <label for="contact">Contact Number</label>
+                            <input type="text" id="contact" name="contact" placeholder="Contact Number" value="">
+                            <div class="error"></div>
+                        </div>
+                     </div>
+                     <div class="row">
+                        <div class="group">
+                            <label for="address_1">House No./Unit No./Building/Street</label>
+                            <input type="text" id="address_1" name="address_1" placeholder="House No./Unit No./Building/Street" value="">
+                            <div class="error"></div>
+                        </div>
+                        <div class="group">
+                            <label for="address_2">Barangay</label>
+                            <select id="address_2" name="address_2">
+                            <option value="" disabled selected>Select Barangay...</option>
+                            <?php
+                                $barangays = array(
+                                    'Aguso',
+                                    'Alvindia',
+                                    'Amucao',
+                                    'Armenia',
+                                    'Asturias',
+                                    'Atioc',
+                                    'Balanti',
+                                    'Balete',
+                                    'Balibago I',
+                                    'Balibago II',
+                                    'Balingcanaway',
+                                    'Banaba',
+                                    'Bantog',
+                                    'Baras-baras',
+                                    'Batang-batang',
+                                    'Binauganan',
+                                    'Bora',
+                                    'Buenavista',
+                                    'Buhilit',
+                                    'Burot',
+                                    'Calingcuan',
+                                    'Capehan',
+                                    'Carangian',
+                                    'Care',
+                                    'Central',
+                                    'Culipat',
+                                    'Cut-cut I',
+                                    'Cut-cut II',
+                                    'Dalayap',
+                                    'Dela Paz',
+                                    'Dolores',
+                                    'Laoang',
+                                    'Ligtasan',
+                                    'Lourdes',
+                                    'Mabini',
+                                    'Maligaya',
+                                    'Maliwalo',
+                                    'Mapalacsiao',
+                                    'Mapalad',
+                                    'Matatalaib',
+                                    'Paraiso',
+                                    'Poblacion',
+                                    'Salapungan',
+                                    'San Carlos',
+                                    'San Francisco',
+                                    'San Isidro',
+                                    'San Jose',
+                                    'San Jose de Urquico',
+                                    'San Juan Bautista',
+                                    'San Juan de Mata',
+                                    'San Luis',
+                                    'San Manuel',
+                                    'San Miguel',
+                                    'San Nicolas',
+                                    'San Pablo',
+                                    'San Pascual',
+                                    'San Rafael',
+                                    'San Roque',
+                                    'San Sebastian',
+                                    'San Vicente',
+                                    'Santa Cruz',
+                                    'Santa Maria',
+                                    'Santo Cristo',
+                                    'Santo Domingo',
+                                    'Santo Niño',
+                                    'Sapang Maragul',
+                                    'Sapang Tagalog',
+                                    'Sepung Calzada',
+                                    'Sinait',
+                                    'Suizo',
+                                    'Tariji',
+                                    'Tibag',
+                                    'Tibagan',
+                                    'Trinidad',
+                                    'Ungot',
+                                    'Villa Bacolor'
+                                );
+                                foreach ($barangays as $barangay){
+                                    echo "<option value='$barangay'>$barangay</option>";
+                                }
+                            ?>
+                            </select>
+                            <div class="error"></div>
+                        </div>
+                     </div>
                 </div>
                 <div class="frame wide">
                     <p class="title">Pin Location</p>
-                    
                     <div id="map"></div>
+                    <input type="submit" value="Create Profile">
+                    </form>
                 </div>
             </div>
-                    <input type="submit" value="Create">
-                </form>
         </div>
     </div>
 </body>
