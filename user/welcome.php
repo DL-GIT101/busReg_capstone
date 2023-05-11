e4d976d137e02b0062dd3a50fb9b9b88cf79d7f8
donlHenard <pastor.git101@gmail.com>
2023-05-11 22:11:13 +0800

    Add select data and display datas

--- user/welcome.php
+++ user/welcome.php
@@ -1,3 +1,55 @@
+<?php 
+session_start();
+
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+require_once "../php/config.php";
+
+$sql = "SELECT * FROM user_profile WHERE user_id = ?";
+
+    if($stmt = $mysqli->prepare($sql)){
+        $stmt->bind_param("s",$param_id);
+
+        $param_id = validate($_SESSION['id']);
+
+        if($stmt->execute()){
+            $result = $stmt->get_result();
+
+            if($result->num_rows == 1){
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+
+                $logo_path = "upload/".$row["logo"];
+                $business_name = $row["business_name"];
+                $name = $row["first_name"]." ".$row["middle_name"]." ".$row["last_name"];
+                $business_activity = $row["activity"];
+                $permit_status = $row["permit_status"];
+                $latitude = $row["latitude"];
+                $longitude = $row["longitude"];
+
+            }else {
+                header("location: profile.php");
+                exit();
+            }
+        }else{
+            echo "Oops! Something went wrong. Please try again later";
+        }
+
+    }
+
+    $stmt->close();
+    
+    $mysqli->close();
+
+    function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+            return $data;
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,6 +65,7 @@
      crossorigin=""></script>
     <script src="../js/map.js" defer></script>
     <script src="../js/displayMap.js" defer></script>
+    <script src="../js/profile.js" defer></script>
     <title>Welcome</title>
 </head>
 <body>
@@ -35,23 +88,19 @@
             <div id="profile">
                  <div class="frame" >
                     <p class="sentence">Business Profile</p>
-                    <img src="../img/Tarlac_City_Seal.png" alt="Tarlac_City_Seal">
+                    <img src="<?= $logo_path ?>" alt="Logo">
                     <div>
-                        <p class="title">Store Name</p>
-                        <p class="sentence">Name of Owner</p> 
+                        <p class="title"><?= $business_name ?></p>
+                        <p class="sentence"><?= $name ?></p> 
                     </div>
                 </div>
                 <div class="frame">
                     <p class="sentence">Business Activity</p> 
-                    <div class="item title">
-                        Money Laundering
-                    </div>
+                    <div class="item title"><?= $business_activity ?></div>
                 </div>
                 <div class="frame">
                     <p class="sentence">Business Permit Status</p> 
-                    <div class="item title approved">
-                        Approved
-                    </div>
+                    <div class="item title " id="permit_status"><?= $permit_status ?></div>
                 </div>
             </div>
             
@@ -68,7 +117,8 @@
                 <div class="frame wide">
                     <p class="title">Location</p>
                     <div id="map">
-                        <!--temp- map -->
+                        <p id="latitude" class="hidden"><?= $latitude ?></p>
+                        <p id="longitude" class="hidden"><?= $longitude ?></p>
                     </div>
                 </div>
             </div>
