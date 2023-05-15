bb8d01cd742fe7e99e8717d5a1dc1a0f6dec4b11
donlHenard <pastor.git101@gmail.com>
2023-05-15 16:43:55 +0800

    Add to query input type email and password

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
 const errors = document.querySelectorAll('form .error');
-const inputs = document.querySelectorAll('form input[type=text], form select,form input[type=file], #map');
-const select = document.querySelectorAll('form select');
+const inputs = document.querySelectorAll('form input[type=email],form input[type=password], form input[type=text], form select,form input[type=file], #map');
 let i = 0;
+console.log(inputs);
 errors.forEach(error => {
   if(error.textContent.trim().length > 0){
     inputs[i].style.boxShadow = '0 0 3px red'
