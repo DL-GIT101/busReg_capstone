16fee9988953fbeda1c586771357992ab54af66d
donlHenard <pastor.git101@gmail.com>
2023-05-19 23:57:56 +0800

    Add onclick for closing and opening modal

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,12 @@
-const modal = document.getElementById('notif_modal');
+const notif_modal = document.getElementById('notif_modal');
 const close_btn = document.getElementById('modal_close_btn');
 
-close_btn.onclick = () => modal.style.display = "none";
+close_btn.onclick = () => notif_modal.style.display = "none";
+
+const delete_file_btns = document.querySelectorAll('.delete_file');
+delete_file_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        notif_modal.style.display = "block";
+      
+    });console.log(btn);
+  });
