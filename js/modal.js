c5daee8a1da18c7c2caf754a439a3af5424475df
donlHenard <pastor.git101@gmail.com>
2023-05-21 22:06:09 +0800

    Add feature to change href

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,6 @@
 const notif_modal = document.getElementById('notif_modal');
 const close_btn = document.getElementById('modal_close_btn');
+const file_link = document.getElementById('file_link');
 
 close_btn.onclick = () => notif_modal.style.display = "none";
 
@@ -7,6 +8,7 @@ const delete_file_btns = document.querySelectorAll('.delete_file');
 delete_file_btns.forEach(btn => {
     btn.addEventListener('click', () => {
         notif_modal.style.display = "block";
+        file_link.href = "delete_file.php?file=" + encodeURIComponent(btn.value);
       
     });console.log(btn);
   });
