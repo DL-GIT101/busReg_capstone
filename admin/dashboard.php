c00fa30a0a1b771e06381c526245b8851b5d2b53
donlHenard <pastor.git101@gmail.com>
2023-05-23 21:55:57 +0800

    Add html containers for data

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,7 +5,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== "admin"){
     header("location: ../login.php");
     exit;
 }
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -33,20 +34,70 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== "admin"){
 
     <nav id="sidebar">
         <ul>
-            <li><img src="../img/dashboard.png" alt=""><a href="dashboard.php">Dashboard</a></li>
-            <li><img src="../img/list.png" alt=""><a href="">MSME List</a></li>
-            <li><img src="../img/register.png" alt=""><a href="">Register MSME</a></li>
+            <li class="current"><img src="../img/dashboard.png" alt=""><a href="dashboard.php">Dashboard</a></li>
+            <li><img src="../img/register.png" alt=""><a href="">MSME Management</a></li>
+            <li><img src="../img/list.png" alt=""><a href="">MSME Permit</a></li>
+            
         </ul>
     </nav>
 
     <div id="content">
 
-        <div class="container"> 
-            
-        
+      
+        <div id="profile">
+                 <div class="frame" >
+                    <p class="sentence">Users</p>   
+                    <div class="item title row space-between">
+                        <p>Total</p>
+                        <p>0</p>
+                    </div>  
+                    <div class="item approved title row space-between">
+                        <p>Profile</p>
+                        <p>0</p>
+                    </div>     
+                </div>
+                <div class="frame">
+                    <p class="sentence">Permit</p> 
+                    <div class="item pending title row space-between">
+                        <p>Pending</p>
+                        <p>0</p>
+                    </div>  
+                    <div class="item approved title row space-between">
+                        <p>Approved</p>
+                        <p>0</p>
+                    </div>
+                    <div class="item denied title row space-between">
+                        <p>Denied</p>
+                        <p>0</p>
+                    </div> 
+                    <div class="item none title row space-between">
+                        <p>None</p>
+                        <p>0</p>
+                    </div>      
+                </div>
+                <div class="frame">
+                    <p class="sentence">Documents</p> 
+                    <div class="item pending title row space-between">
+                        <p>Incomplete</p>
+                        <p>0</p>
+                    </div>  
+                    <div class="item approved title row space-between">
+                        <p>Complete</p>
+                        <p>0</p>
+                    </div>
+                    <div class="item none title row space-between">
+                        <p>None</p>
+                        <p>0</p>
+                    </div>        
+                </div>
+        </div>
+        <div class="frame wide">
+                    <p class="title">Business Location</p>
+                    <div id="map"></div>
+                </div>
         </div>
 
-    </div>
+   
 </div>
 
 </body>
