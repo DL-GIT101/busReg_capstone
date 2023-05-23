4ac60b7f5b61fd092fd748252816a92b0a485645
donlHenard <pastor.git101@gmail.com>
2023-05-23 10:30:11 +0800

    Add navbar

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -11,9 +11,23 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== "admin"){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Dashboard</title>
 </head>
 <body>
-    <h1>admin</h1>
+<nav id="navbar">
+    <div id="logo">
+        <a href="../admin/dashboard.php">
+            <img src="../img/Tarlac_City_Seal.png" alt="Tarlac_City_Seal">
+            <p>Business Permit & Licensing</p> 
+        </a>
+    </div>
+    <p>ADMIN</p> 
+    <div id="user">
+        <a href="../php/logout.php">Logout</a>
+    </div>
+</nav>
+
+
 </body>
 </html>
