18d2ae895fb1dfd8157c9cdb17a1a9fa5cf81bd9
donlHenard <pastor.git101@gmail.com>
2023-05-25 20:57:36 +0800

    Add file for clickable rows
    get the table row and add event in which get the data from id then
    create url from that lastly redirect using that url

---
+++ js/click_row.js
@@ -0,0 +1,12 @@
+const rows = document.querySelectorAll(".user_info");
+
+rows.forEach( row => {
+    row.addEventListener('click', () => {
+        let id = row.cells[0].innerText;
+        
+        let url = "msme_profile.php?id=" + encodeURIComponent(id);
+
+        window.location.href = url;
+    })
+});
+
