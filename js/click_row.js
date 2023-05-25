d916032fd93e581c9fd39888561c160c393c16ed
donlHenard <pastor.git101@gmail.com>
2023-05-25 21:14:35 +0800

    Add if else for user without profile

--- js/click_row.js
+++ js/click_row.js
@@ -1,12 +1,16 @@
 const rows = document.querySelectorAll(".user_info");
 
 rows.forEach( row => {
-    row.addEventListener('click', () => {
-        let id = row.cells[0].innerText;
-        
-        let url = "msme_profile.php?id=" + encodeURIComponent(id);
-
-        window.location.href = url;
-    })
+    if(row.cells[1].innerText === "Created"){
+        row.addEventListener('click', () => {
+            let id = row.cells[0].innerText;
+            
+            let url = "msme_profile.php?id=" + encodeURIComponent(id);
+    
+            window.location.href = url;
+        })
+    }else {
+        row.classList.remove('user_info');
+    }
 });
 
