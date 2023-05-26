d53c1e7c88eb923fff3e859ca917b74ee4f3369c
donlHenard <pastor.git101@gmail.com>
2023-05-26 15:02:09 +0800

    Remove if else statement

--- js/click_row.js
+++ js/click_row.js
@@ -1,16 +1,12 @@
 const rows = document.querySelectorAll(".user_info");
 
 rows.forEach( row => {
-    if(row.cells[1].innerText === "Created"){
-        row.addEventListener('click', () => {
+    row.cells[0].addEventListener('click', () => {
             let id = row.cells[0].innerText;
             
             let url = "msme_profile.php?id=" + encodeURIComponent(id);
     
             window.location.href = url;
         })
-    }else {
-        row.classList.remove('user_info');
-    }
 });
 
