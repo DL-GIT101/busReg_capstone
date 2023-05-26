96fb6a6762a3986e000190ff580450bb5a861bac
donlHenard <pastor.git101@gmail.com>
2023-05-26 22:12:27 +0800

    Add  pointer
    To remove the property in the css and to be used by other class.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const nav_logo = document.getElementById('nav_logo');
-
+nav_logo.style.cursor = 'pointer';
 nav_logo.addEventListener('click', () => {
     window.location.href = '../index.php';
 });
