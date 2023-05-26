d6001f0b441bfe99bf17ae24332ec91fe786f26b
donlHenard <pastor.git101@gmail.com>
2023-05-27 00:23:02 +0800

    Change for each to for loop
    With the index logic i decided it better be for loop

--- js/form.js
+++ js/form.js
@@ -1,15 +1,19 @@
-const errors = document.querySelectorAll('form .error');
+const error_msg = document.querySelectorAll('form .error_msg');
 const inputs = document.querySelectorAll('form input[type=email],form input[type=password], form input[type=text], form select,form input[type=file], #map');
-let i = 0;
-console.log(inputs);
-errors.forEach(error => {
-  if(error.textContent.trim().length > 0){
-    inputs[i].style.boxShadow = '0 0 3px red'
-    inputs[i].style.border = '1px solid red'
-    inputs[i].style.marginBottom = '0px'
+
+
+for (let i = 0; i < error_msg.length; i++) {
+  if(error_msg[i].textContent.trim().length > 0){
+    inputs[i].classList.add('error_input');
   }
-    i+=1;
-});
+}
+
+for (let i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('input', () => {
+    inputs[i].classList.remove('error_input');
+    error_msg[i].classList.add('hidden');
+  });  
+}
 
 const alerts = document.querySelectorAll('form .alert');
 alerts.forEach(alert => {
