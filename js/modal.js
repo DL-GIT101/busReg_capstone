89c5e7c702aac8be71338a291f1e0840a8d8e8b2
donlHenard <pastor.git101@gmail.com>
2023-05-28 21:31:31 +0800

    Recode the modal

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,12 @@
+const modal = document.querySelector('modal');
+const close_btn = document.querySelector('modal > .content > button');
+
+close_btn.addEventListener('click', () => {
+  modal.style.display = "none";
+});
+
 const notif_modal = document.getElementById('notif_modal');
 const file_link = document.getElementById('file_link');
-
-const modal = document.querySelectorAll('.modal');
-const close_btn = document.querySelectorAll('.modal_close_btn, .modal a');
-close_btn.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        modal[index].style.display = "none";      
-    });console.log(btn);console.log(modal[index]);
-  });
-
 const delete_file_btns = document.querySelectorAll('.delete_file');
 delete_file_btns.forEach(btn => {
     btn.addEventListener('click', () => {
