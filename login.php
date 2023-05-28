b3f4bd63537f19e5fb3e18be7b3fd1e7e68fc037
donlHenard <pastor.git101@gmail.com>
2023-05-28 11:17:56 +0800

    Recode the hhtml
    Adjust the spacing in phph

--- login.php
+++ login.php
@@ -2,50 +2,44 @@
 
 session_start();
 
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     header("location: user/dashboard.php");
     exit;
 }
 
 require_once "php/connection.php";
 
-$email = $email_err = $password = $pword_error = "";
-
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST") {
 //email
-    if(empty(validate($_POST["email"]))){
+    if(empty(validate($_POST["email"]))) {
         $email_err = "Please enter your email"; 
-    } else{
+    } else {
         $email = validate($_POST["email"]);
     }
 //password
-    if(empty(validate($_POST["password"]))){
+    if(empty(validate($_POST["password"]))) {
         $pword_error = "Please enter your password";
     }
-    else{
+    else {
         $password = validate($_POST["password"]);
     }
 
-    if(empty($email_err) && empty($pword_error)){
+    if(empty($email_err) && empty($pword_error)) {
 
         $sql = "SELECT id, email, password FROM users WHERE email = ?";
 
         if($stmt = $mysqli->prepare($sql)){
-
             $stmt->bind_param("s", $param_email);
 
             $param_email = $email;
 
-            if($stmt->execute()){
-
+            if($stmt->execute()) {
                 $stmt->store_result();
 
-                if($stmt->num_rows == 1){
-
+                if($stmt->num_rows === 1) {
                     $stmt->bind_result($id,$email,$hashed_pword);
 
                     if($stmt->fetch()){
-
                         if(password_verify($password, $hashed_pword)){
                             session_start();
 
@@ -58,7 +52,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                                 header("location: admin/dashboard.php");
                             }else{
                                 $_SESSION["loggedin"] = true;
-                                header("location: user/welcome.php");
+                                header("location: user/dashboard.php");
                             }
 
                         }else{
@@ -69,13 +63,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     $login_err = "Invalid email or password";
                 }
             }else{
-                echo "error in sql";
+                echo "Error executing query";
             }
-
-            $stmt->close();
+        $stmt->close();
         }
     }
-        $mysqli->close();
+$mysqli->close();
 }
 
 function validate($data) {
@@ -93,6 +86,7 @@ function validate($data) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <!-- Javascript -->
+    <script src="js/script.js" defer></script>
     <script src="js/form.js" defer></script>
     <title>Login</title>
 </head>
@@ -115,14 +109,14 @@ function validate($data) {
                 <p class="sentence">Sign in to your account to continue</p>
             </div>
             <form autocomplete="off" method="post" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                <div class="alert"><?= $login_err; ?></div>
+                <div class="error_alert hidden"><?= $login_err; ?></div>
                 <label for="email">Email</label>
                 <input type="email" id="email" name="email" placeholder="Email Address" value=<?= $email; ?>>
-                <div class="error"><?= $email_err; ?></div>
+                <div class="error_msg"><?= $email_err; ?></div>
 
                 <label for="password">Password</label>
                 <input type="password" id="password" name="password" placeholder="Password" value=<?= $password; ?>>
-                <div class="error"><?= $pword_error; ?></div>
+                <div class="error_msg"><?= $pword_error; ?></div>
 
                 <input type="submit" value="Login">
                 <a href="user/register.php">Don't have an account? Click Here</a>
