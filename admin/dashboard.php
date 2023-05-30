8478f2b3e34c6e6e5effadee1322f7311fea6246
donlHenard <pastor.git101@gmail.com>
2023-05-30 21:43:21 +0800

    Recode the dashboard admin

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== "admin"){
     header("location: ../login.php");
     exit;
 }
-require_once "../php/config.php";
+require_once "../php/connection.php";
 
 $sql1 = "SELECT COUNT(*) FROM users WHERE id <> ?";
     if($stmt1 = $mysqli->prepare($sql1)){
@@ -75,89 +75,89 @@ if ($stmt3 = $mysqli->prepare($sql3)) {
      <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
      integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
      crossorigin=""></script>
+    <script src="../js/script.js" defer></script>
     <script src="../js/map.js" defer></script>
     <script src="../js/businessLocation.js" defer></script>
     <title>Dashboard</title>
 </head>
 <body>
-<nav id="navbar">
-<p>ADMIN</p> 
-    <div id="logo">
-        <a href="../index.php">
-            <img src="../img/Tarlac_City_Seal.png" alt="Tarlac_City_Seal">
-            <p>Business Permit & Licensing</p> 
-        </a>
-    </div>
-    
-    <div id="user">
-        <a href="../php/logout.php">Logout</a>
-    </div>
-</nav>
+    <nav>
+        <div id="nav_logo">
+                <img src="../img/Tarlac_City_Seal.png" alt="Tarlac City Seal">
+                <p>Tarlac City Business Permit & Licensing Office</p>  
+        </div>
+        <div id="account">
+             <a href="../php/logout.php">Logout</a>
+        </div>
+    </nav>
 
-<div class="row">
+<div class="flex">
 
     <nav id="sidebar">
         <ul>
             <li class="current"><img src="../img/dashboard.png" alt=""><a href="dashboard.php">Dashboard</a></li>
             <li><img src="../img/register.png" alt=""><a href="user_management.php">MSME Management</a></li>
             <li><img src="../img/list.png" alt=""><a href="">MSME Permit</a></li>
-            
         </ul>
     </nav>
 
-    <div id="content">
-        <div id="profile">
-                 <div class="frame" >
+    <main class="flex-grow-1">
+        <content>
+            <section>
+                 <subsection class="space-around">
                     <p class="sentence">Users</p>   
-                    <div class="item title row space-between">
+                    <div class="info title flex space-between">
                         <p>Total</p>
                         <p><?= $userCount ?></p>
                     </div>  
-                    <div class="item approved title row space-between">
+                    <div class="info approved title flex space-between">
                         <p>Profile</p>
                         <p><?= $profileCount ?></p>
                     </div>     
-                </div>
-                <div class="frame">
+                </subsection>
+                <subsection class="space-around">
                     <p class="sentence">Documents</p> 
-                    <div class="item pending title row space-between">
+                    <div class="info pending title flex space-between">
                         <p>Incomplete</p>
                         <p><?= $incompleteCount ?></p>
                     </div>  
-                    <div class="item approved title row space-between">
+                    <div class="info approved title flex space-between">
                         <p>Complete</p>
                         <p><?= $completeCount ?></p>
                     </div>
-                    <div class="item none title row space-between">
+                    <div class="info none title flex space-between">
                         <p>None</p>
                         <p><?= $noneReqCount ?></p>
                     </div>        
-                </div>
-                <div class="frame">
+                </subsection>
+                <subsection class="space-around">
                     <p class="sentence">Permit</p> 
-                    <div class="item pending title row space-between">
+                    <div class="info pending title flex space-between">
                         <p>Pending</p>
                         <p>0</p>
                     </div>  
-                    <div class="item approved title row space-between">
+                    <div class="info approved title flex space-between">
                         <p>Approved</p>
                         <p>0</p>
                     </div>
-                    <div class="item denied title row space-between">
+                    <div class="info denied title flex space-between">
                         <p>Denied</p>
                         <p>0</p>
                     </div> 
-                    <div class="item none title row space-between">
+                    <div class="info none title flex space-between">
                         <p>None</p>
                         <p>0</p>
                     </div>      
-                </div>
-        </div>
-        <div class="frame wide">
+                </subsection>
+            </section>
+            <section class="flex-grow-15">
+                <subsection>
                     <p class="title">Business Location</p>
-                    <div id="map"></div>
-        </div>
-    </div>
+                    <map id="map"></map>
+                </subsection>
+            </section>
+        </content>
+    </main>
 </div>
 
 </body>
