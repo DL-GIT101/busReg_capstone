b718179961f95ea2d9935a777c3554eab5faf28e
donlHenard <pastor.git101@gmail.com>
2023-05-30 16:37:16 +0800

    Delete the fn for files

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,6 @@
 const modal = document.querySelector('modal');
-const close_btn = document.querySelector('modal > .content > button');
+const close_btn = document.querySelector('modal > .content > .close');
 
 close_btn.addEventListener('click', () => {
   modal.style.display = "none";
 });
-
-const notif_modal = document.getElementById('notif_modal');
-const file_link = document.getElementById('file_link');
-const delete_file_btns = document.querySelectorAll('.delete_file');
-delete_file_btns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        notif_modal.style.display = "block";
-        file_link.href = "delete_file.php?file=" + encodeURIComponent(btn.value);
-    });
-  });
