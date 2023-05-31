d85302ef62ddee71c70633e93268d2eff6fdf790
donlHenard <pastor.git101@gmail.com>
2023-05-31 20:19:24 +0800

    Recode the html elements

--- admin/msme_profile.php
+++ admin/msme_profile.php
@@ -22,7 +22,7 @@ $sql = "SELECT * FROM user_profile WHERE user_id = ?";
                 if (!empty($row["logo"])) {
                     $logo_path = "../user/upload/".$user_id."/".$row["logo"];
                 } else {
-                    $logo_path = "../userupload/No_image_available.svg";
+                    $logo_path = "../user/upload/No_image_available.svg";
                 }
                 $business_name = $row["business_name"];
                 $name = $row["first_name"]." ".$row["middle_name"]." ".$row["last_name"];
@@ -87,130 +87,68 @@ function validate($data) {
      <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
      integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
      crossorigin=""></script>
+     <script src="../js/script.js" defer></script>
     <script src="../js/map.js" defer></script>
     <script src="../js/displayMap.js" defer></script>
-    <script src="../js/profile.js" defer></script>
-    <title>User Profile</title>
+    <title>MSME Profile</title>
 </head>
 <body>
-<nav id="navbar">
-<p>ADMIN</p> 
-    <div id="logo">
-        <a href="../index.php">
-            <img src="../img/Tarlac_City_Seal.png" alt="Tarlac_City_Seal">
-            <p>Business Permit & Licensing</p> 
-        </a>
-    </div>
-    
-    <div id="user">
-        <a href="../php/logout.php">Logout</a>
-    </div>
-</nav>
+    <nav>
+        <div id="nav_logo">
+                <img src="../img/Tarlac_City_Seal.png" alt="Tarlac City Seal">
+                <p>Tarlac City BPLO - ADMIN</p>  
+        </div>
+        <div id="account">
+             <a href="../php/logout.php">Logout</a>
+        </div>
+    </nav>
 
-<div class="row">
+<div class="flex">
 
     <nav id="sidebar">
         <ul>
             <li ><img src="../img/dashboard.png" alt=""><a href="dashboard.php">Dashboard</a></li>
-            <li class="current"><img src="../img/register.png" alt=""><a href="user_management.php">MSME Management</a></li>
+            <li class="current"><img src="../img/register.png" alt=""><a href="msme_management.php">MSME Management</a></li>
             <li><img src="../img/list.png" alt=""><a href="">MSME Permit</a></li>
             
         </ul>
     </nav>
 
-    <div id="content flex-column">
-        <div class="container">
-
-            <p class="title"><?= $user_id ?></p>
-            <div class="actions">
-                <button class="delete_btn">Delete</button>
-                <button class="edit_btn">Edit</button>
-            </div>
-
-        </div>
-
-        <div class="container flex-row <?= $hidden ?>"> 
-            
-            <div id="profile">
-                 <div class="frame" >
+    <main class="flex-grow-1">
+        <content>
+            <section class="flex-grow-2">
+                <subsection class="space-around">
                     <p class="sentence">Business Profile</p>
-                    <div class="logo_container"> 
+                    <div id="logo"> 
                         <img src="<?= $logo_path ?>" alt="Logo">
                     </div>
-                    <div>
+                    <div class="text-center">
                         <p class="title"><?= $business_name ?></p>
                         <p class="sentence"><?= $name ?></p> 
                     </div>
-                </div>
-                <div class="frame">
+                </subsection>
+                <subsection class="space-around">
                     <p class="sentence">Business Activity</p> 
-                    <div class="item title"><?= $business_activity ?></div>
-                </div>
-                <div class="frame">
+                    <div class="info title"><?= $business_activity ?></div>
+                </subsection>
+                <subsection class="space-around">
                     <p class="sentence">Business Permit Status</p> 
-                    <div class="item title " id="permit_status"><?= $permit_status ?></div>
-                </div>
-            </div>
-            
-                <div class="frame wide">
-                    <p class="title">Location</p>
-                    <div id="map">
-                        <p id="latitude" class="hidden"><?= $latitude ?></p>
-                        <p id="longitude" class="hidden"><?= $longitude ?></p>
-                    </div>
-                </div>
-        </div>
-        <div class="container <?= $hidden ?>"> 
-        
-    <div class="intro">
-        <p class="title">Uploaded Documents</p>
-    </div>
-        <table>
-            <tr>
-                <th>Requirement</th>
-                <th>View</th>
-                <th>Status</th>
-            </tr>
-            <?php 
-                $requirements_names = array(
-                    'Barangay Clearance for business',
-                    'DTI Certificate of Registration',
-                    'On the Place of Business',
-                    'Community Tax Certificate',
-                    'Certificate of Zoning Compliance',
-                    'Business Inspection Clearance',
-                    'Valid Fire Safety Inspection Certificate/Official Receipt',
-                    'Sanitary Permit',
-                    'Environmental Compliance Clearance',
-                    'Latest 2x2 picture',
-                    'Tax Order of Payment',
-                    'Tax Order of Payment Official Receipt'
-                );
-                $count = 1;
-                foreach($requirements_names as $fileName){
-                        $errorMsg = 'errorMsg_' . $count;
-                    echo '  <tr>
-                                <td>'.$fileName.'</td>';
-
-                    if(empty($requirements_fetch[$count-1])){
-                      echo '<td></td>
-                            <td></td>
-                            ';
-                    }else{
-                        echo    '<td><a class="view_file" target="_blank" href="../user/upload/'.$user_id.'/'.$requirements_fetch[$count-1].'">View</a></td>
-                        <td>'.$status_fetch[$count-1].'</td>';
-                    }
-                        echo '</tr>';
-                $count++;
-                }
-            ?>
-            
-        </table>     
-    </div>
-    </div>
+                    <div class="info title <?= strtolower($permit_status)?>" id="permit_status"><?= $permit_status ?></div>
+                </subsection>
+            </section>
+            <section class="flex-grow-15">
+                <subsection>
+                        <p class="title">Location</p>
+                        <map id="map">
+                            <p id="latitude" class="hidden"><?= $latitude ?></p>
+                            <p id="longitude" class="hidden"><?= $longitude ?></p>
+                        </map>
+                </subsection>
+            </section>
+        </content>
+    </main>
 
 </div>
-    </div>
-</div>
+
 </body>
 </html>
