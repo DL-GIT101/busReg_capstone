a0477b5ae76c0b9827cc51f200a6292c4abc44cd
donlHenard <pastor.git101@gmail.com>
2023-06-08 21:56:43 +0800

    Change the naming of links for logo
    The level of folder determined by slashes in the file path.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 const nav_logo = document.getElementById('nav_logo');
-const page = document.querySelector('title');
+
+const filePath = window.location.pathname;
+const directoryPath = filePath.substring(0, filePath.lastIndexOf('/'));
+const pathLevels = directoryPath.split('/').length - 1;
 
 nav_logo.style.cursor = 'pointer';
 nav_logo.addEventListener('click', () => {
     
-    if(page.innerText === "Login" || page.innerText === "BPLO"){
+    if(pathLevels === 1){
         window.location.href = 'index.php';
-    }else{
+    }else if(pathLevels === 2){
         window.location.href = '../index.php';
+    }else if(pathLevels === 3){
+        window.location.href = '../../index.php';
     }
 });
