d8931f3627fe3aaedb363ec464df0ff9ce054f24
donlHenard <pastor.git101@gmail.com>
2023-06-08 14:33:16 +0800

    Fix bug
    When denied was chosen only the last message was displayed. i moved
    the denied var to eventlistner then boom.

--- js/form.js
+++ js/form.js
@@ -1,7 +1,6 @@
 const error_msg = document.querySelectorAll('form .error_msg');
 const inputs = document.querySelectorAll('form input[type=email],form input[type=password], form input[type=text], form select,form input[type=file], #map');
 
-
 for (let i = 0; i < error_msg.length; i++) {
   if(error_msg[i].textContent.trim().length > 0){
     inputs[i].classList.add('error_input');
@@ -23,35 +22,38 @@ error_alert.forEach(alert => {
 });
 
 const select_review  = document.querySelectorAll('.select_review');
+const denied_message  = document.querySelectorAll('.denied_message');
 
-if(select_review){
+if(select_review && denied_message){
 
-  const colorReview = (review) => {
-    let selected = review.options[review.selectedIndex];
-    review.className = "";
-    review.className = "select_review";
-      if(selected.innerText == "Uploaded"){
-        review.classList.add('uploaded');
-      }else if(selected.innerText == "Pending"){
-        review.classList.add('pending');
-      }else if(selected.innerText == "Denied"){
-        review.classList.add('denied');
-      }else if(selected.innerText == "Approved"){
-        review.classList.add('approved');
-      }
-  };
-
-    select_review.forEach(review => {
+  select_review.forEach((review, index )=> {
       if(review.value == "Uploaded"){
         review.classList.add('uploaded');
       }else if(review.value == "Pending"){
         review.classList.add('pending');
       }else if(review.value == "Denied"){
-        review.classList.add('denied');
+        review.classList.add('denied');   
       }else if(review.value == "Approved"){
         review.classList.add('approved');
       }
-      review.addEventListener('change', () => colorReview(review));
+
+      review.addEventListener('change', () => {
+        let selected = review.options[review.selectedIndex];
+        review.className = "select_review";
+
+        denied = denied_message[index];
+        denied.className = "denied_message hidden";
+
+          if(selected.innerText == "Uploaded"){
+            review.classList.add('uploaded');
+          }else if(selected.innerText == "Pending"){
+            review.classList.add('pending');      
+          }else if(selected.innerText == "Denied"){
+            review.classList.add('denied');
+            denied.classList.remove('hidden');
+          }else if(selected.innerText == "Approved"){
+            review.classList.add('approved');   
+          }
+      });
   });
 };
-
