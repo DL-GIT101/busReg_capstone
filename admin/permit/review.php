fc083469e05b933f92c91b4f56c569ac09b6e3d6
donlHenard <pastor.git101@gmail.com>
2023-06-09 21:57:42 +0800

    Add profile with all info

--- admin/permit/review.php
+++ admin/permit/review.php
@@ -11,17 +11,54 @@ if(isset($_GET['id'])){
 }
 $modal = "hidden";
 
+$sql = "SELECT * FROM user_profile WHERE user_id = ?";
 
-    $sql = "SELECT * FROM new_documents WHERE user_id = ?";
-   
     if($stmt = $mysqli->prepare($sql)){
-        
         $stmt->bind_param("s",$param_id);
+
+        $param_id = $user_id;
+
+        if($stmt->execute()){
+            $result = $stmt->get_result();
+
+            if($result->num_rows == 1){
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+
+                if (!empty($row["logo"])) {
+                    $logo_path = "../../user/upload/".$user_id."/".$row["logo"];
+                } else {
+                    $logo_path = "../../img/No_image_available.svg";
+                }
+                $business_name = $row["business_name"];
+                $name = $row["first_name"]." ".$row["middle_name"]." ".$row["last_name"]." ".$row["suffix"];
+                $business_activity = $row["activity"];
+                $permit_status = $row["permit_status"];
+                $latitude = $row["latitude"];
+                $longitude = $row["longitude"];
+                $gender = $row['gender'];
+                $contact = $row['contact_number'];
+                $address = $row["address_1"]." ".$row["address_2"];
+
+            }else{
+                $profile = "hidden";
+                $none = "";
+            }
+        }else{
+            echo "Oops! Something went wrong. Please try again later";
+        }
+        $stmt->close();
+    }
+
+    $sql2 = "SELECT * FROM new_documents WHERE user_id = ?";
+   
+    if($stmt2 = $mysqli->prepare($sql2)){
+        
+        $stmt2->bind_param("s",$param_id);
         
         $param_id = $user_id;
        
-        if($stmt->execute()){
-            $result = $stmt->get_result();
+        if($stmt2->execute()){
+            $result = $stmt2->get_result();
             if($result->num_rows == 1){
            
                $row = $result->fetch_array(MYSQLI_ASSOC);
@@ -38,7 +75,7 @@ $modal = "hidden";
         }else {
             echo "error retrieving data";
         }
-    $stmt->close();
+    $stmt2->close();
     }
     
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -124,12 +161,20 @@ function validate($data) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+     integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+     crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+     integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+     crossorigin=""></script>
     <script src="../../js/script.js" defer></script>
+    <script src="../../js/map.js" defer></script>
+    <script src="../../js/displayMap.js" defer></script>
     <script src="../../js/form.js" defer></script>
     <script src="../../js/modal.js" defer></script>
     <script src="../../js/file_modal.js" defer></script>
-    <script src="../../js/admin_td_click.js" defer></script>
-    <title>Denied Documents</title>
+    <script src="../../js/contentTwo.js" defer></script>
+    <title>Review Information</title>
 </head>
 <body>
 <p id="user_id" class="hidden"><?= $user_id ?></p>
@@ -167,23 +212,66 @@ function validate($data) {
     <nav id="sidebar">
         <ul>
             <li ><img src="../../img/dashboard.png" alt=""><a href="../dashboard.php">Dashboard</a></li>
-            <li class="current"><img src="../../img/register.png" alt=""><a href="../management/users.php">MSME Management</a></li>
-            <li><img src="../../img/list.png" alt=""><a href="">MSME Permit</a></li>
+            <li ><img src="../../img/register.png" alt=""><a href="../management/users.php">MSME Management</a></li>
+            <li class="current"><img src="../../img/list.png" alt=""><a href="permit.php">MSME Permit</a></li>
             
         </ul>
     </nav>
 
     <main class="flex-grow-1 flex-wrap content-center">
+
     <div class="actions space-between">
-            <p id="page" class="title">Review Documents</p>
+            <p id="page" class="title">Review</p>
             <p class="sentence"> User ID : <?= $user_id ?></p>
             <div class="buttons">
-                <a href="../management/documents.php" class="back">Documents</a>
-                <a href="documents.php" class="back">Documents</a>
+                <a href="msme.php" class="back">List</a>
+
+                <a href="../management/edit_profile.php">Edit</a>
+                <a href="" class="back">Document</a>
+                
+                <a href="../management/documents.php">Upload</a>
+                <a href="" class="back">Profile</a>
             </div>
         </div>
 
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" enctype="multipart/form-data">
+        <content>
+            <section class="flex-grow-2">
+                <subsection class="space-around">
+                    <p class="sentence">Business Profile</p>
+                    <div id="logo"> 
+                        <img src="<?= $logo_path ?>" alt="Logo">
+                    </div>
+                    <div class="text-center">
+                        <p class="title"><?= $business_name ?></p>
+                        <p class="sentence"><?= $business_activity ?></p> 
+                    </div>
+                </subsection>
+                <subsection class="space-around">
+                    <p class="sentence">Business Permit Status</p> 
+                    <div class="info title <?= strtolower($permit_status)?>" id="permit_status"><?= $permit_status ?></div>
+                </subsection>
+                <subsection class="space-around">
+                    <p class="sentence">Business Owner</p> 
+                    <div class="text-center">
+                        <p class="title"><?= $name ?></p> 
+                        <p class="sentence"><?= $gender ?></p> 
+                        <p class="sentence"><?= $contact ?></p>
+                    </div>
+                </subsection>
+            </section>
+            <section class="flex-grow-15">
+                <subsection>
+                        <p class="title">Location</p>
+                        <p class="sentence"><?= $address ?></p>
+                        <map id="map">
+                            <p id="latitude" class="hidden"><?= $latitude ?></p>
+                            <p id="longitude" class="hidden"><?= $longitude ?></p>
+                        </map>
+                </subsection>
+            </section>
+        </content>
+
+    <form class="hidden" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" enctype="multipart/form-data">
             <div class="text-center">
                 <p class="sentence">Review the following documents carefully</p>
             </div>
