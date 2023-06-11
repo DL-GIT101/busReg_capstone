49ff8f8d0ff8bcfaebb24727343a609a0099fe42
donlHenard <pastor.git101@gmail.com>
2023-06-11 21:54:50 +0800

    Move the file

---
+++ php/get_coordinates.php
@@ -0,0 +1,20 @@
+<?php 
+require_once "connection.php";
+$sql = "SELECT business_name, longitude, latitude FROM user_profile";
+if ($stmt = $mysqli->prepare($sql)) {
+    if ($stmt->execute()) {
+        $stmt->bind_result($name,$longitude, $latitude);
+
+        $coordinates = array();
+        while ($stmt->fetch()) {
+            $coordinates[] = array('name' => $name, 'longitude' => $longitude, 'latitude' => $latitude);
+        }
+        $coordinatesJson = json_encode($coordinates);
+        echo $coordinatesJson;
+    } else {
+        echo "Error retrieving data";
+    }
+    $stmt->close();
+}
+$mysqli->close();
+?>
