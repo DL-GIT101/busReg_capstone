5311e8e7349be1300672d06ffba144429ea3ecc1
donlHenard <pastor.git101@gmail.com>
2023-06-20 17:20:36 +0800

    Add functions to this
    Plan to put all modal feartures heare

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,40 @@
-const modal = document.querySelector('modal');
-const close_btn = document.querySelector('modal > .content > .close');
+const modals = document.querySelectorAll('modal');
+const close_btn = document.querySelectorAll('modal .close');
 
 if(close_btn){
-  close_btn.addEventListener('click', () => {
-    modal.style.display = "none";
+  close_btn.forEach(btn => {
+      btn.addEventListener('click', () => {
+          modals.forEach(modal => {
+            modal.style.display = "none";
+          });
+      });
+  });
+};
+
+const delete_modal = document.querySelector('modal.delete');
+const delete_link = document.querySelector('modal.delete a');
+const delete_btn = document.querySelectorAll('table img.delete');
+
+if(delete_modal && delete_btn){
+  delete_btn.forEach(btn => {
+    btn.addEventListener('click', () => {
+      delete_modal.style.display = "flex";
+      let td = btn.parentNode;
+      let view = td.firstElementChild;
+      let href = view.getAttribute('href');
+      let lastIndex = href.lastIndexOf('/');
+      let fileName = href.substring(lastIndex + 1);
+
+      delete_link.href = "../php/deleteFile.php?file=" + encodeURIComponent(fileName);
+    });
+  });
+};
+
+const info_modal = document.querySelector('modal.info');
+const info_btn = document.querySelector('table img.info');
+
+if(info_modal && info_btn){
+  info_btn.addEventListener('click', () => {
+    info_modal.style.display = "flex";
   });
 };
