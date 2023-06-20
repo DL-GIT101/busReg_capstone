ce9719c05cc1f0ec53790351d1013fb674f8dde6
donlHenard <pastor.git101@gmail.com>
2023-06-20 19:42:11 +0800

    For status svg feature

---
+++ js/table.js
@@ -0,0 +1,23 @@
+const fileStatusMap = {
+    Uploaded: '../img/uploaded.svg',
+    Approved: '../img/approved.svg',
+    Denied: '../img/denied.svg',
+    Pending: '../img/pending.svg'
+  };
+  
+  const fileStatusElements = document.querySelectorAll('table td .status');
+  
+  if (fileStatusElements) {
+    fileStatusElements.forEach(file => {
+      const status = file.innerText;
+      if (fileStatusMap.hasOwnProperty(status)) {
+        file.innerText = '';
+        const img = document.createElement('img');
+        img.src = fileStatusMap[status];
+        img.alt = status;
+        file.appendChild(img);
+      }
+    });
+  }
+
+  
