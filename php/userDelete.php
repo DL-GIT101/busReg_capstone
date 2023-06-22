b32c11581b844c67629252830fb32b541e75b317
donlHenard <pastor.git101@gmail.com>
2023-06-22 13:14:10 +0800

    Add new features

--- php/userDelete.php
+++ php/userDelete.php
@@ -1,62 +1,130 @@
-<?php 
+<?php ini_set('display_errors', 1);
 session_start();
-require_once "../../php/connection.php";
-require_once "../../php/functions.php";
-require_once "../../php/checkPermit.php";
+require_once "connection.php";
+require_once "functions.php";
+require_once "checkPermit.php";
 
-if(isset($_GET['id_user'])){
-    $user_id = urldecode($_GET['id_user']);
+if(checkRole($_SESSION["role"]) !== "admin"){
+    header("location: ../index.php");
+    exit;
+}
+
+if(isset($_GET['user'])){
+
+    $user_id = urldecode($_GET['user']);
     $sql = "DELETE FROM users WHERE id = ?";
     $page = "management";
+    $link = "../admin/management/users.php";
+
 }else if(isset($_GET['profile'])){
+
     $user_id = validate($_GET['profile']);
     $sql = "DELETE user_profile, new_documents
         FROM user_profile
         LEFT JOIN new_documents ON user_profile.user_id = new_documents.user_id
         WHERE user_profile.user_id = ?";
     $page = "profile";
+    $link = "../admin/management/profiles.php";
+
 }else if(isset($_GET['documents'])){
+
     $user_id = validate($_GET['documents']);
     $sql = "DELETE FROM new_documents WHERE user_id = ?";
     $page = "documents";
+    $link = "../admin/management/documents.php";
+
 }else{
-    header("location: ../management/users.php");
+    header("location: ../admin/management/users.php");
 }
 
-$userDirectory = '../../user/upload/' . $user_id;
-if(checkPermit($mysqli) !== "Approved"){
- if ($stmt = $mysqli->prepare($sql)) {
+$userDirectory = '../user/upload/' . $user_id;
+$error = "";
+
+if(checkPermit($user_id) !== "Approved"){
+
+    if ($stmt = $mysqli->prepare($sql)) {
 
         $stmt->bind_param("s", $param_id);
         $param_id = $user_id;
+
         if ($stmt->execute()) {
         
             deleteDirectory($userDirectory,$page);
-            if($page === "profile"){
-                header("location: ../management/profiles.php?id=".$user_id);
-            }else if($page === "management") {
-                header("location: ../management/users.php");
+
+            if($page === "management"){
+
+                $title = "User has been deleted";
+        
+            }else if($page === "profile") {
+                
+                $title = "Profile has been deleted";
+        
             } else if($page === "documents"){
-                header("location: ../management/documents.php?id=".$user_id);
+                
+                $title = "All Docuements has been deleted";
             }
+        
+            $message = '<modal>
+                            <div class="content success">
+                                <p class="title">'.$title.'</p>
+                                <div class="button-group">
+                                    <button class="close">Close</button>
+                                </div>
+                            </div>
+                        </modal>
+            ';
+            header('location: '.$link.'?message='. urlencode($message));
         } else {
-            echo "Error deleting user";
-            header("location: ../management/users.php");
+            $error = "yes";
         }
-        $stmt->close();
+    }else {
+        $error = "yes";
     }
 
+    $stmt->close();
+
+    if($error === "yes"){
+    
+        $message = '<modal>
+                        <div class="content error">
+                            <p class="title">Something went wrong</p>
+                            <p class="sentence">Try again later</p>
+                            <div class="button-group">
+                                <button class="close">Close</button>
+                            </div>
+                        </div>
+                    </modal>
+        ';
+        header('location: '.$link.'?message='. urlencode($message));
+    }
 $mysqli->close(); 
 }else{
+
+    if($page === "management"){
+
+        $title = "User cannot be deleted";
+
+    }else if($page === "profile") {
+
+        $title = "Profile cannot be deleted";
+
+    } else if($page === "documents"){
+
+        $title = "Documents cannot be deleted";
+
+    }
+
     $message = '<modal>
                     <div class="content success">
-                        <p class="title">Documents cannot be deleted</p>
+                        <p class="title">'.$title.'</p>
                         <p class="sentence">The permit has already been approved</p>
-                        <a href="../management/profiles.php">OK</a>
+                        <div class="button-group">
+                            <button class="close">Close</button>
+                        </div>
                     </div>
                 </modal>
     ';
-    header("location: ../management/profiles.php?message=". urlencode($message));
+    header('location: '.$link.'?message='. urlencode($message));
 }
 
 function deleteDirectory($directory,$page) {
