1378938096dd9edd0fb199f686aeacf7177e690e
donlHenard <pastor.git101@gmail.com>
2023-06-23 15:27:57 +0800

    Move checkPermit fn to functions.php

--- php/functions.php
+++ php/functions.php
@@ -18,4 +18,70 @@ function checkRole($role) {
     }
 }
 
+function hasProfile($id){
+
+    global $mysqli;
+
+    $sql = "SELECT * FROM user_profile WHERE user_id = ?";
+
+    if($stmt = $mysqli->prepare($sql)){
+        $stmt->bind_param("s",$param_id);
+
+        $param_id = $id;
+
+        if($stmt->execute()){
+            $result = $stmt->get_result();
+
+            if($result->num_rows === 1){
+                return true;
+            }else {
+                return false;
+            }
+
+        }else{
+            return "Error";
+        }
+
+    $stmt->close();
+
+    }else {
+
+        return "Error";
+        $stmt->close();
+    }
+}
+
+function checkPermit($id){
+    global $mysqli;
+
+    $sql = "SELECT status FROM permit WHERE user_id = ?";
+
+    if($stmt = $mysqli->prepare($sql)){
+        $stmt->bind_param("s",$param_id);
+
+        $param_id = $id;
+
+        if($stmt->execute()){
+            $result = $stmt->get_result();
+
+            if($result->num_rows === 1){
+                $row = $result->fetch_assoc();
+                $status = $row['status'];
+                return $status;
+            }else{
+                return "None";
+            }
+        }else{
+            return "Error";
+        }
+
+    $stmt->close();
+
+    }else {
+
+        return "Error";
+        $stmt->close();
+    }
+
+}
 ?>
