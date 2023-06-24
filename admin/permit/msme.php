5248799636f7345b037e05d77b863d5fbcef84b1
donlHenard <pastor.git101@gmail.com>
2023-06-24 21:07:45 +0800

    Restyled and add new column

--- admin/permit/msme.php
+++ admin/permit/msme.php
@@ -1,13 +1,19 @@
 <?php
 session_start();
+require_once "../../php/connection.php";
+require_once "../../php/functions.php";
 
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== "admin"){
-    header("location: ../../login.php");
+if(checkRole($_SESSION["role"]) !== "admin"){
+    header("location: ../../index.php");
     exit;
 }
 
-require_once "../../php/connection.php";
-require_once "../../php/validate.php";
+if(isset($_GET['message'])){
+    $modal_get = urldecode($_GET['message']);
+    echo $modal_get;
+}
+
+$modal_display = "hidden";
 
 $all_business = array();
 
@@ -99,7 +105,7 @@ $user_sql = "SELECT users.id FROM users
 
                             $business['documents'] = $completedRequirements . '/' . $totalRequirements;
                         } else {
-                            $business['documents'] = "None";
+                            $business['documents'] = "0/11";
                         }
                     } else {
                         echo "Error retrieving data";
@@ -120,51 +126,67 @@ $mysqli->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/style.css">
     <script src="../../js/script.js" defer></script>
-    <script src="../../js/admin_td_click.js" defer></script>
+    <script src="../../js/modal.js" defer></script>
+    <script src="../../js/table.js" defer></script>
     <title>MSME List</title>
 </head>
 <body>
-<modal id="user_del" class="hidden">
-        <div class="content fail">
-            <p class="title">Delete File</p>
-            <p class="sentence">Are you sure you want to delete this user? This action cannot be undone</p>
-            <div id="btn_grp" class="flex align-self-center">
-                <a href="" id="user_link">Delete</a>
-                <button>Cancel</button>
+    <modal class="<?= $modal_display ?>">
+        <div class="content <?= $modal_status ?>">
+            <p class="title"><?= $modal_title ?></p>
+            <p class="sentence"><?= $modal_message ?></p>
+            <div class="button-group">
+                <?= $modal_button ?>
             </div>
         </div>
-</modal>
-<nav>
-        <div id="nav_logo">
-                <img src="../../img/Tarlac_City_Seal.png" alt="Tarlac City Seal">
-                <p>Tarlac City BPLO - ADMIN</p>  
-        </div>
-        <div id="account">
-             <a href="../../php/logout.php">Logout</a>
-        </div>
-</nav>
+    </modal>
 
-<div class="flex">
+    <nav>
+        <div class="logo">
+            <img src="../../img/Tarlac_City_Seal.png" alt="Tarlac City Seal">
+            <p>Tarlac City Business Permit & Licensing Office</p>  
+        </div>
+        <img id="toggle" src="../../img/navbar-toggle.svg" alt="Navbar Toggle">
+        <div class="button-group">
+            <ul>
+                <li><a href="../dashboard.php">Dashboard</a></li>
+                <li><a href="../management/users.php">Management</a></li>
+                <li class="current"><a href="msme.php">Permit</a></li>
+                <li><a href="../../php/logout.php">Logout</a></li>
+            </ul>
+            <ul id="subnav-links">
+                <li class="current"><a href="msme.php">List</a></li>
+                <li><a href="review.php">Review</a></li>
+                <li><a href="approve.php">Approve</a></li>
+            </ul>
+        </div>
+    </nav>
 
-    <nav id="sidebar">
-        <ul>
-            <li ><img src="../../img/dashboard.png" alt=""><a href="../dashboard.php">Dashboard</a></li>
-            <li ><img src="../../img/register.png" alt=""><a href="../management/users.php">MSME Management</a></li>
-            <li class="current"><img src="../../img/list.png" alt=""><a href="">MSME Permit</a></li>
-        </ul>
+    <nav id="subnav">
+        <div class="logo">
+            <img src="../../img/admin.svg" alt="Tarlac City Seal">
+            <p>Admin</p>  
+        </div>
+        <div class="button-group">
+            <ul>
+                <li class="current"><a href="msme.php">List</a></li>
+                <li><a href="review.php">Review</a></li>
+                <li><a href="approve.php">Approve</a></li>
+            </ul>
+        </div>
     </nav>
 
     <main class="flex-grow-1">
-        <content class="justify-center">
-            <div class="column_container"> 
-                    <p id="page" class="title text-center">MSME List</p>
-                <table>   
+        <div class="column-container height-auto"> 
+            <p id="page" class="title text-center">MSME List</p>
+                <table id="msme">   
                     <tr>
                         <th>ID</th>
                         <th>Name</th>
                         <th>Activity</th>
                         <th>Documents</th>
                         <th>Permit</th>
+                        <th>Review</th>
                     </tr>
                     <?php 
                     foreach ($all_business as &$business) {
@@ -172,16 +194,15 @@ $mysqli->close();
                                     <td>'.$business['id'].'</td>
                                     <td>'.$business['name'].'</td>
                                     <td>'.$business['activity'].'</td>
-                                    <td><div class="info none">'.$business['documents'].'</div></td>
-                                    <td><div class="info '.strtolower($business['permit']) .'">'.$business['permit'].'</div></td>
+                                    <td><div class="data">'.$business['documents'].'</div></td>
+                                    <td><div class="data">'.$business['permit'].'</div></td>
+                                    <td><img src="../../img/review.svg" alt="Review"></td>
                                 </tr>';
                 }
                     ?>
                 </table>
-            </div>
-        </content>
+        </div>
     </main>
-</div>
 
 </body>
 </html>
