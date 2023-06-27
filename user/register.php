892dd7165d03885a2d3972ac32cc23ba72e94f45
donlHenard <pastor.git101@gmail.com>
2023-06-27 15:37:52 +0800

    add icon

--- user/register.php
+++ user/register.php
@@ -174,6 +174,8 @@ $mysqli->close();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/tarlac-seal.ico" type="image/x-icon">
+    <link rel="icon" href="img/tarlac-seal.ico" type="image/x-icon">
     <link rel="stylesheet" href="../css/style.css">
     <!-- Javascript -->
     <script src="../js/script.js" defer></script>
