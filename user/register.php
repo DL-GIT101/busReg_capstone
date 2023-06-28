c8217f5f7eb20f2b6538a1bd291c4c41a1478060
donlHenard <pastor.git101@gmail.com>
2023-06-28 12:44:30 +0800

    Rename again

--- user/register.php
+++ user/register.php
@@ -3,10 +3,10 @@ session_start();
 require_once "../php/connection.php";
 require_once "../php/functions.php";
 
-if(checkRole($_SESSION["role"]) === "user") {
+if($_SESSION["role"] === "user") {
     header("location: dashboard.php");
     exit;
-}elseif(checkRole($_SESSION["role"]) === "admin") {
+}elseif($_SESSION["role"] === "admin") {
     header("location: ../admin/dashboard.php");
     exit;
 }
